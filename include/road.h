8bbd11974ed8221393da5ee84556adb5713cb634
Matthias Maile <matthias.maile@pm.me>
2022-04-15 21:37:47 +0900

    removed boggus road

--- include/road.h
+++ include/road.h
@@ -6,26 +6,6 @@
 #include <car.h>
 #include <vector>
 
-class Road {
-    protected:
-    Car* cars;
-    unsigned int car_number = 0;
-    float length = 1000.;
-
-    public:
-    Road() = default;
-    Road(unsigned int const car_num, float const len);
-    ~Road();
-
-
-    float distance(unsigned int car1, unsigned int car2);
-    int find_car_in_front(unsigned int const input);
-
-    int set_car_number(unsigned int new_car_number);
-
-    void RK4(float dt);
-};
-
 class OneLaneRoad {
     protected:
     std::vector<Car> cars;
