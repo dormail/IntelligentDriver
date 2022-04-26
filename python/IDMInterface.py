6bf13693d8a1d9c56464a5acb39b933710db72cb
Matthias Maile <matthias.maile@pm.me>
2022-04-26 22:02:07 +0900

    Doxygen comment added

--- python/IDMInterface.py
+++ python/IDMInterface.py
@@ -7,24 +7,26 @@ import os
 import pandas as pd
 
 
-IDMBinaryName = "./build/main"
+
 BUILD_DIR = "build"
 
-def IDMSimulation(road_length=200, cars=5, lanes=3, step_width=1/30, steps=1800):
-  filename = BUILD_DIR + "/" + str(road_length) + "_" + str(cars) + "_" + str(lanes) + "_" + str(step_width) + "_" + str(steps) + ".csv"
+## A function acting as a interface between python code and the multi lane idm implementation
+# Special care needs to be addressed for the directory structure, so BUILD_DIR and IDMBinaryName
+# @return [filename, df] filename is the name of the CSV file generated, df is the dataframe with the data
+def IDMSimulation(road_length=200, cars=5, lanes=3, step_width=1/30, steps=1800, IDMBinaryName = "./build/main"):
+    filename = BUILD_DIR + "/" + str(road_length) + "_" + str(cars) + "_" + str(lanes) + "_" + str(step_width) + "_" + str(steps) + ".csv"
 
-  options = " --road-length " + str(road_length)
-  options = options + " --cars " + str(cars)
-  options = options + " --lanes " + str(lanes)
-  options = options + " --step-width " + str(step_width)
-  options = options + " --integration-steps " + str(steps)
-  options = options + " --output-csv " + str(filename)
+    options = " --road-length " + str(road_length)
+    options = options + " --cars " + str(cars)
+    options = options + " --lanes " + str(lanes)
+    options = options + " --step-width " + str(step_width)
+    options = options + " --integration-steps " + str(steps)
+    options = options + " --output-csv " + str(filename)
 
-  print(IDMBinaryName + options)
+    #print(IDMBinaryName + options)
 
-  os.system(IDMBinaryName + options)
-  df = pd.read_csv(filename)
-  return df
+    os.system(IDMBinaryName + options)
+    df = pd.read_csv(filename, index_col=False)
+    return [filename, df]
 
-IDMSimulation()
 
