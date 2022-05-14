2f22e35cba428d84ea4eded80a85674f62f3f3aa
dormail <dormail@users.noreply.github.com>
2022-05-14 13:47:26 +0000

    deploy: 1c2e89dcf117df039a4f431fd3bc27657b0f993f

--- search/all_0.js
+++ search/all_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['a_5fbias_0',['a_bias',['../classMultiLaneRoad.html#af0f901f5bec37fc67bd620f4c69dd2c8',1,'MultiLaneRoad']]],
   ['a_5fc_5feur_1',['a_c_eur',['../classMultiLaneRoad.html#a00808ae17cb80897ceeb0234b3970e18',1,'MultiLaneRoad']]],
   ['accel_2',['accel',['../structCar.html#a9ad6366e32f251577396c0d565e69436',1,'Car']]],
-  ['acceleration_3',['acceleration',['../road_8cpp.html#abadc4a8663a3befb28e91f7c21a99908',1,'acceleration(float v, float v_next, float distance, float v_max, float s_min, float T, float a_max, float b):&#160;road.cpp'],['../road_8h.html#abadc4a8663a3befb28e91f7c21a99908',1,'acceleration(float v, float v_next, float distance, float v_max, float s_min, float T, float a_max, float b):&#160;road.cpp']]],
-  ['acceleration_5fcar_4',['acceleration_car',['../classMultiLaneRoad.html#a3ee286c38fa1875b44dd5aac788ea879',1,'MultiLaneRoad']]],
+  ['acceleration_3',['acceleration',['../road_8cpp.html#abadc4a8663a3befb28e91f7c21a99908',1,'acceleration(float v, float v_next, float distance, float v_max, float s_min, float T, float a_max, float b):&#160;road.cpp'],['../road_8h.html#a4087c4682ab1bd7f2862015b7cb11511',1,'acceleration(float const v, float const v_next, float const distance, float const v_max, float const s_min, float const T, float const a_max, float const b):&#160;road.cpp']]],
+  ['acceleration_5fcar_4',['acceleration_car',['../classMultiLaneRoad.html#a3ee286c38fa1875b44dd5aac788ea879',1,'MultiLaneRoad::acceleration_car(const Car &amp;car, const Car &amp;car_front)'],['../classMultiLaneRoad.html#aa1d256847a57d329c640b22f563b8877',1,'MultiLaneRoad::acceleration_car(const Car &amp;car, const Car *car_front)']]],
   ['average_5fspeed_5',['average_speed',['../classOneLaneRoad.html#ab24a9e3adb2bba5951d070e30e0c3ebe',1,'OneLaneRoad']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['fill_5fright_5flanes_0',['fill_right_lanes',['../classMultiLaneRoad.html#a2bc2ce065ae8e47f35a3cc506fb05824',1,'MultiLaneRoad']]],
-  ['follower_1',['follower',['../classMultiLaneRoad.html#a40face7cbc3edd362204f2cdc4ff8ce0',1,'MultiLaneRoad']]]
+  ['follower_1',['follower',['../classMultiLaneRoad.html#a40face7cbc3edd362204f2cdc4ff8ce0',1,'MultiLaneRoad']]],
+  ['free_5facceleration_2',['free_acceleration',['../road_8cpp.html#ad097530fcc9b74c4d3940df1fa057947',1,'free_acceleration(const Car &amp;car):&#160;road.cpp'],['../road_8h.html#ad097530fcc9b74c4d3940df1fa057947',1,'free_acceleration(const Car &amp;car):&#160;road.cpp']]]
 ];
