c5d26dbadf5968c3667dc1a9aeae0d205dcb6e56
dormail <dormail@users.noreply.github.com>
2022-05-15 00:47:43 +0000

    deploy: 66c9b2dc2293201efc0908d1b0ae3b994f387c5e

--- search/functions_0.js
+++ search/functions_0.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['a_5fc_5feur_0',['a_c_eur',['../classMultiLaneRoad.html#a00808ae17cb80897ceeb0234b3970e18',1,'MultiLaneRoad']]],
   ['acceleration_1',['acceleration',['../road_8cpp.html#abadc4a8663a3befb28e91f7c21a99908',1,'acceleration(float v, float v_next, float distance, float v_max, float s_min, float T, float a_max, float b):&#160;road.cpp'],['../road_8h.html#a4087c4682ab1bd7f2862015b7cb11511',1,'acceleration(float const v, float const v_next, float const distance, float const v_max, float const s_min, float const T, float const a_max, float const b):&#160;road.cpp']]],
-  ['acceleration_5fcar_2',['acceleration_car',['../classMultiLaneRoad.html#a3ee286c38fa1875b44dd5aac788ea879',1,'MultiLaneRoad::acceleration_car(const Car &amp;car, const Car &amp;car_front)'],['../classMultiLaneRoad.html#aa1d256847a57d329c640b22f563b8877',1,'MultiLaneRoad::acceleration_car(const Car &amp;car, const Car *car_front)']]],
+  ['acceleration_5fcar_2',['acceleration_car',['../classMultiLaneRoad.html#a3ee286c38fa1875b44dd5aac788ea879',1,'MultiLaneRoad::acceleration_car(const Car &amp;car, const Car &amp;car_front)'],['../classMultiLaneRoad.html#aa1d256847a57d329c640b22f563b8877',1,'MultiLaneRoad::acceleration_car(const Car &amp;car, const Car *car_front)'],['../classMultiLaneRoad.html#a8796a5b420928f6d91ae45f33731b3ce',1,'MultiLaneRoad::acceleration_car(const Car *car, const Car *car_front)']]],
   ['average_5fspeed_3',['average_speed',['../classOneLaneRoad.html#ab24a9e3adb2bba5951d070e30e0c3ebe',1,'OneLaneRoad']]]
 ];
